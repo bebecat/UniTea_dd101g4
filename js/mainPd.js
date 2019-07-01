30b03b06a2a89747b6e5406d8dbe843910e41764
paradisemrch <tabooyu@gmail.com>
2019-07-01 20:08:29 +0800

    pdList動畫未好

---
+++ js/mainPd.js
@@ -0,0 +1,5 @@
+console.log("start");
+TweenMax.to(".fly_animal", 4, {
+  x: -200,
+  y: 500
+});
---
+++ js/output/gulpfile.js
@@ -0,0 +1,7 @@
+const { src, dest } = require("gulp");
+
+function copy() {
+  return src("./*.js").pipe(dest("js/output/"));
+}
+
+exports.copy = copy;
