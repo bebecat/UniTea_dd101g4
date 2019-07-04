a107e151cc93f488686317cb8672c3d4d7a7dbba
Ouu Lola <lola453338@gmail.com>
2019-07-04 11:17:12 +0800

    增加機器人、修改關於我們、動畫準備中

---
+++ au_js/aboutus.js
@@ -0,0 +1,7 @@
+function doFirst(){
+	var treeleft = document.getElementsByClassName("tree")[0];
+	var treeright = document.getElementsByClassName("tree")[1];
+	
+}
+
+window.addEventListener("load",doFirst,false);
