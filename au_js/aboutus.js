570d39df63ff1a45288aeded8b7af7b5f0b85a5f
Ouu Lola <lola453338@gmail.com>
2019-07-04 16:49:02 +0800

    痛苦的RWD中

--- au_js/aboutus.js
+++ au_js/aboutus.js
@@ -1,7 +1,17 @@
 function doFirst(){
-	var treeleft = document.getElementsByClassName("tree")[0];
-	var treeright = document.getElementsByClassName("tree")[1];
-	
+	//var treeleft = document.getElementsByClassName("tree")[0];
+	//var treeright = document.getElementsByClassName("tree")[1];
+	//treeleft.
+//適應不同瀏覽器問題
+
+	//loading後停一秒延遲，第一屏開始動畫		
+	//樹叢從中間分開到兩側，自己動，約一秒
+
+	//標題從中間的放大位置縮小到中間原本靜態位置，與樹叢同時動作，也是一秒
+
+	//第一屏滑滾輪自動換屏到第二屏，大約是
+
+	//第二屏的container也是滑鼠換屏
 }
 
 window.addEventListener("load",doFirst,false);
