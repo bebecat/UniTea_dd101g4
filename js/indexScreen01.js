8d814e687bc826f2c704951e56b44a35c899122b
Gengyu <s0912758806p@gmail.com>
2019-07-08 00:46:58 +0800

    // Gengyu - indexUpdate16

--- js/indexScreen01.js
+++ js/indexScreen01.js
@@ -1,6 +1,4 @@
 var parallaxMove = document.getElementById('parallaxMove');
-var grassMoveA = document.getElementById('grassMoveA');
-var grassMoveB = document.getElementById('grassMoveB');
 var tweezersMove = document.getElementById('tweezersMove');
 var txtMove = document.getElementById('itemtxt');
 var smobjectMoveA = document.getElementById('smobjectA');
@@ -16,9 +14,6 @@ function parallax(e){
     var y = mouseY * -0.2 / 8;
     
     parallaxMove.style.transform ='translate('+ x + 'px,' + y + 'px)';
-    grassMoveA.style.transform = 'translate(' + (x / 2) + 'px,' + (y / 2) + 'px)';
-    grassMoveB.style.transform = 'translate(' + (x + 1) + 'px,' + (y + 1) + 'px)';
-    
     tweezersMove.style.transform = 'translate('+ (x / 3 ) + 'px,' + (y / 3) + 'px)';
     txtMove.style.transform = 'translate(' + ( x / 3 ) + 'px,' + (y / 3) + 'px)';
 
