10e661133dc3191308c169dd6a1e3a03b6d2ce66
Gengyu <s0912758806p@gmail.com>
2019-07-10 18:08:45 +0800

    // Gengyu - indexUpdate21

--- js/indexScreen01.js
+++ js/indexScreen01.js
@@ -1,10 +1,10 @@
 var parallaxMove = document.getElementById('parallaxMove');
 var tweezersMove = document.getElementById('tweezersMove');
 var txtMove = document.getElementById('itemtxt');
-var smobjectMoveA = document.getElementById('smobjectA');
-var smobjectMoveB = document.getElementById('smobjectB');
-var smobjectMoveC = document.getElementById('smobjectC');
-var smobjectMoveD = document.getElementById('smobjectD');
+// var smobjectMoveA = document.getElementById('smobjectA');
+// var smobjectMoveB = document.getElementById('smobjectB');
+// var smobjectMoveC = document.getElementById('smobjectC');
+// var smobjectMoveD = document.getElementById('smobjectD');
 
 function parallax(e){
     var mouseX = e.clientX
@@ -17,10 +17,10 @@ function parallax(e){
     tweezersMove.style.transform = 'translate('+ (x / 3 ) + 'px,' + (y / 3) + 'px)';
     txtMove.style.transform = 'translate(' + ( x / 3 ) + 'px,' + (y / 3) + 'px)';
 
-    smobjectMoveA.style.transform = 'translate(' + (x + 2) + 'px,' + (y + 2) + 'px)';
-    smobjectMoveB.style.transform = 'translate(' + (x + 2) + 'px,' + (y + 2) + 'px)';
-    smobjectMoveC.style.transform = 'translate(' + (x + 2) + 'px,' + (y + 2) + 'px)';
-    smobjectMoveD.style.transform = 'translate(' + (x + 2 ) + 'px,' + (y + 2 ) + 'px)';
+    // smobjectMoveA.style.transform = 'translate(' + (x + 2) + 'px,' + (y + 2) + 'px)';
+    // smobjectMoveB.style.transform = 'translate(' + (x + 2) + 'px,' + (y + 2) + 'px)';
+    // smobjectMoveC.style.transform = 'translate(' + (x + 2) + 'px,' + (y + 2) + 'px)';
+    // smobjectMoveD.style.transform = 'translate(' + (x + 2 ) + 'px,' + (y + 2 ) + 'px)';
     // console.log(mouseX, mouseY);
     
 }
