f8e2f96c6614c8695af9e954699fec160629422e
Ouu Lola <lola453338@gmail.com>
2019-07-12 14:52:58 +0800

    整合首頁(footer有爆)、商品頁、活動頁(尚未完成)、關於我們

--- js/indexScreen01.js
+++ js/indexScreen01.js
@@ -1,7 +1,10 @@
 var parallaxMove = document.getElementById('parallaxMove');
-var grassMoveA = document.getElementById('grassMoveA');
-var grassMoveB = document.getElementById('grassMoveB');
 var tweezersMove = document.getElementById('tweezersMove');
+var txtMove = document.getElementById('itemtxt');
+// var smobjectMoveA = document.getElementById('smobjectA');
+// var smobjectMoveB = document.getElementById('smobjectB');
+// var smobjectMoveC = document.getElementById('smobjectC');
+// var smobjectMoveD = document.getElementById('smobjectD');
 
 function parallax(e){
     var mouseX = e.clientX
@@ -11,10 +14,14 @@ function parallax(e){
     var y = mouseY * -0.2 / 8;
     
     parallaxMove.style.transform ='translate('+ x + 'px,' + y + 'px)';
-    grassMoveA.style.transform = 'translate(' + (x / 2) + 'px,' + (y / 2) + 'px)';
-    grassMoveB.style.transform = 'translate(' + (x + 1) + 'px,' + (y + 1) + 'px)';
-    tweezersMove.style.transform = 'translate('+ (x / 3 ) + 'px,' + (y / 3) + 'px)';
-    // console.log(mouseX, mouseY);
+    tweezersMove.style.transform = 'translate('+ (x + 2 ) + 'px,' + (y + 2 ) + 'px)';
+    txtMove.style.transform = 'translate(' + ( x / 3 ) + 'px,' + (y / 3) + 'px)';
+
+    // smobjectMoveA.style.transform = 'translate(' + (x + 2) + 'px,' + (y + 2) + 'px)';
+    // smobjectMoveB.style.transform = 'translate(' + (x + 2) + 'px,' + (y + 2) + 'px)';
+    // smobjectMoveC.style.transform = 'translate(' + (x + 2) + 'px,' + (y + 2) + 'px)';
+    // smobjectMoveD.style.transform = 'translate(' + (x + 2 ) + 'px,' + (y + 2 ) + 'px)';
+    console.log(tweezersMove, parallaxMove);
     
 }
 
